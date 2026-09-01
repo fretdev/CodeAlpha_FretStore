448bf04e74cf8e43ee4e0a8206fde72cdf5ac4c2
Fretdev <ahmedibrahimprosper22@gmail.com>
2026-09-01 06:05:13 +0100

    temporory frontend to test cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import express from 'express'
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
 
+app.use(cors({
+    origin: ["http://127.0.0.1:5500"]
+}))
+
+app.post("/form",(req,res)=>{
+    const {username,password} = req.body
+
+    console.log(`User logged in with: ${username}`)
+    return res.json({
+        username,
+        password
+    })
+})
 
 
 export default app
