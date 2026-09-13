b84fe16cbbee62bbf864f261032b320ae61ae145
Fretdev <ahmedibrahimprosper22@gmail.com>
2026-09-13 23:19:54 +0100

    feat: serve frontend pages with express

--- src/app.js
+++ src/app.js
@@ -4,10 +4,21 @@ import categoriesRouter from "./features/categories/categories.routes.js"
 import authRouter from "./features/auth/auth.routes.js"
 import cartRouter from "./features/cart/cart.routes.js"
 import orderRouter from "./features/orders/order.routes.js"
+import path from "path"
+import { fileURLToPath } from "url"
+
 const app = express()
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 app.use(express.json())
-app.use(express.static("public"))
+
+app.use(express.static(path.join(__dirname,"../public")))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/pages/index.html"))
+})
 
 
 app.use("/api/products",productsRouter)
