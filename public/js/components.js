72aaed78d4fe4c95cea1ce2cf389cef0c14503d7
Fretdev <ahmedibrahimprosper22@gmail.com>
2026-09-15 02:57:06 +0100

    feat: complete navbar for desktop screens

--- public/js/components.js
+++ public/js/components.js
@@ -4,4 +4,15 @@ fetch("/components/navbar.html")
     .then(response => response.text())
     .then(data =>{
         navbar.innerHTML = data
+
+
+    const navLinks = navbar.querySelectorAll(".nav-links a")
+    navLinks.forEach(link =>{
+       link.addEventListener('click',()=>{
+        navLinks.forEach(link =>{
+            link.classList.remove('active')
+        })
+        link.classList.add("active")
+       })
+    })
     })
