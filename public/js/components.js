2aeda549c3a621be625e03067b142909cf029479
Fretdev <ahmedibrahimprosper22@gmail.com>
2026-09-15 12:36:47 +0100

    feat: complete navbar for tablet devices

--- public/js/components.js
+++ public/js/components.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById("navbar")
 
+
 fetch("/components/navbar.html")
     .then(response => response.text())
     .then(data =>{
@@ -15,4 +16,19 @@ fetch("/components/navbar.html")
         link.classList.add("active")
        })
     })
+    const menuBar = navbar.querySelector('.menu-bar')
+    const navListContainer = navbar.querySelector('.nav-links-container')
+    const navLists = navbar.querySelector('.nav-links')
+    const navButtons = navbar.querySelector('.nav-btns')
+    const loginBtn = navbar.querySelector('.login-btn')
+    const registerBtn = navbar.querySelector('.register-btn')
+    menuBar.addEventListener('click',()=>{
+        menuBar.classList.toggle('is-toggled')
+        navListContainer.classList.toggle('is-toggled')
+        navLists.classList.toggle('is-toggled')
+        navButtons.classList.toggle('is-toggled')
+        loginBtn.classList.toggle('is-toggled')
+        registerBtn.classList.toggle('is-toggled')
+    })
+    
     })
